b3af5b624a43c3453996eccbb93acd74720ccecb
Julien Adam <julien.adam@gmail.com>
2023-11-23 18:31:29 +0100

    Included Revealjs and added basic page generation

--- src/Revealer.Engine/Program.fs
+++ src/Revealer.Engine/Program.fs
@@ -2,44 +2,64 @@
 open FSharp.Formatting.Common
 open Giraffe.ViewEngine
 
-let document =
-    """
+//let document =
+//    """
 
-# Section 1 - Slide 1
+//# Section 1 - Slide 1
 
----
+//---
 
-# Section 1 - Slide 2
+//# Section 1 - Slide 2
 
-***
+//***
 
-# Section 2 - Slide 1
-"""
+//# Section 2 - Slide 1
+//"""
 
-let parsed = Markdown.Parse(document)
+//let parsed = Markdown.Parse(document)
 
-let slides = 
-    parsed.Paragraphs |> List.choose (fun p -> 
-        match p with
-        | HorizontalRule ('-', _) ->
-            div [ _class "section" ] [] |> Some
-        | HorizontalRule ('*', _) ->
-            div [ _class "slide" ] [] |> Some
-        | _ -> None
-        //| Heading (size = 1; body = [ Literal (text = text) ]) ->
-        //    // Recognize heading that has a simple content
-        //    // containing just a literal (no other formatting)
-        //    header [] [ str text] |> Some
-        //| 
-        //| _ -> None
-    )
+//let slides = 
+//    parsed.Paragraphs |> List.choose (fun p -> 
+//        match p with
+//        | HorizontalRule ('-', _) ->
+//            div [ _class "section" ] [ str "section" ] |> Some
+//        | HorizontalRule ('*', _) ->
+//            div [ _class "slide" ] [ str "slide" ] |> Some
+//        | _ -> None
+//        //| Heading (size = 1; body = [ Literal (text = text) ]) ->
+//        //    // Recognize heading that has a simple content
+//        //    // containing just a literal (no other formatting)
+//        //    header [] [ str text] |> Some
+//        //| 
+//        //| _ -> None
+//    )
 
-let view =
-    html [] [
-        head [] [ title [] [ str "Slide test" ] ]
-        body [] slides
+let sectionsAndSlides = [
+        section [] [ str "Section 1"]
+        section [] [ str "Section 2"]
     ]
 
-let result = RenderView.AsString.htmlDocument view
-printfn "%s" result
+let renderRevealHtml pageTitle theme content =
+    let themeCss = sprintf "dist/theme/%s.css" theme;
+    let css = ["dist/reset.css"; "dist/reveal.css"; themeCss; "plugin/highlight/monokai.css"]
+    let scriptRefs = ["dist/reveal.js"; "plugin/notes/notes.js"; "plugin/markdown/markdown.js"; "plugin/highlight/highlight.js"]
+    let initScript = "Reveal.initialize({ hash: true, plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ] });"
 
+    html [ _lang "en"] [
+        head [] [
+            yield meta [ _charset "utf-8"]
+            yield meta [ _name "viewport"; _content "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"]
+            yield! css |> List.map(fun c -> link [ _rel "stylesheet"; _href c])
+            yield title [] [ str pageTitle ]
+        ]
+        body [] [
+            yield div [ _class "reveal"] [ div [_class "slides"] content ]
+            yield! scriptRefs |> List.map(fun s -> script [ _src s ] [])
+            yield script [] [ str initScript ]
+        ]
+    ]
+
+System.IO.File.WriteAllBytes("index.html", 
+    sectionsAndSlides
+    |> renderRevealHtml "Revealer" "beige" 
+    |> RenderView.AsBytes.htmlDocument)
