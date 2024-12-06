6a866a9bec1f9ecceba20851af20fcd94825b78b
Julien Adam <julien.adam@arcodia.fr>
2024-12-06 17:49:35 +0100

    Added logo options for both night and white themes
    various updates to plugins and small css fixes

--- src/Revealer/resources/plugin/mermaid/plugin.js
+++ src/Revealer/resources/plugin/mermaid/plugin.js
@@ -4,52 +4,73 @@
 
 import mermaid from "mermaid/dist/mermaid.js";
 
-const Plugin = {
-  id: "mermaid",
+async function renderMermaid({ el, beforeRender, afterRender }) {
+    const beforeRenderRes = await beforeRender?.(el);
 
-  init: function (reveal) {
-    let { ...mermaidConfig } = reveal.getConfig().mermaid || {};
+    if (beforeRenderRes === false) {
+        return;
+    }
 
-    mermaid.initialize({
-      // The node size will be calculated incorrectly if set `startOnLoad: start`,
-      // so we need to manually render.
-      startOnLoad: false,
-      ...mermaidConfig,
-    });
+    // Using textContent not innerHTML, because innerHTML will get escaped code (eg: get --&gt; instead of -->).
+    const graphDefinition = el.textContent.trim();
 
-    const mermaidEls = reveal.getRevealElement().querySelectorAll(".mermaid");
-
-    Array.from(mermaidEls).forEach(function (el) {
-      var insertSvg = function (svgCode, bindFunctions) {
-        el.innerHTML = svgCode;
-      };
-
-      // Using textContent not innerHTML, because innerHTML will get escaped code (eg: get --&gt; instead of -->).
-      var graphDefinition = el.textContent.trim();
-
-      try {
-        mermaid
-          .render(
+    try {
+        const { svg: svgCode } = await mermaid.render(
             `mermaid-${Math.random().toString(36).substring(2)}`,
             graphDefinition
-          )
-          .then(({ svg }) => {
-            insertSvg(svg);
-          });
-      } catch (error) {
+        );
+        el.innerHTML = svgCode;
+
+        await afterRender?.(el);
+    } catch (error) {
         let errorStr = "";
         if (error?.str) {
-          // From mermaid 9.1.4, error.message does not exists anymore
-          errorStr = error.str;
+            // From mermaid 9.1.4, error.message does not exists anymore
+            errorStr = error.str;
         }
         if (error?.message) {
-          errorStr = error.message;
+            errorStr = error.message;
         }
         console.error(errorStr, { error, graphDefinition, el });
         el.innerHTML = errorStr;
-      }
-    });
-  },
+    }
+}
+
+function getRenderMermaidEl({ beforeRender, afterRender }) {
+    return function renderMermaidEl(el) {
+        return renderMermaid({
+            el,
+            beforeRender,
+            afterRender,
+        });
+    };
+}
+
+const Plugin = {
+    id: "mermaid",
+
+    init: function (reveal) {
+        const { ...mermaidConfig } = reveal.getConfig().mermaid || {};
+        const { ...mermaidPluginConfig } = reveal.getConfig().mermaidPlugin || {};
+
+        const renderMermaidEl = getRenderMermaidEl({
+            beforeRender: mermaidPluginConfig.beforeRender,
+            afterRender: mermaidPluginConfig.afterRender,
+        });
+
+        mermaid.initialize({
+            // The node size will be calculated incorrectly if set `startOnLoad: start`,
+            // so we need to manually render.
+            startOnLoad: false,
+            ...mermaidConfig,
+        });
+
+        const mermaidEls = reveal.getRevealElement().querySelectorAll(".mermaid");
+
+        Array.from(mermaidEls).forEach(function (el) {
+            renderMermaidEl(el);
+        });
+    },
 };
 
 export default () => Plugin;
